89634efb4ff393347fdbbf7c307221fb41acccd9
timchenko@home <vitaliy.timchenko@privatbank.ua>
2022-01-26 03:41:25 +0200

    l04 ex01

---
+++ lesson04/01.js
@@ -0,0 +1,18 @@
+/**
+ * Задача 1
+ * Создать имплементацию функции `forEach`, логика работы такая же как и у родного метода.
+ * Функция должна содержать проверки:
+ * - Первый параметр обязателен и может принимать только массив
+ * - Второй параметр обязателен и может принимать только функцию
+ */
+
+function forEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        callback(array[index], index, array);
+    }
+}
+
+const arr = [1, 2, 3];
+forEach(arr, function (item, i, arr) {
+    console.log(`item: ${item} i: ${i} arr: ${arr}`);
+});
